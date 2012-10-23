842fc8a17ebab1b70357c18426b39c01f590bfad
Samuel Froin <samuelfroin@hotmail.com>
2012-10-23 22:10:37 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyCompet::Application.config.secret_token = '7de905e82ec6e9d8e8bcec3043e52a13cbceab12b508592932cc05cab05dd0af6486f5d49a530b5f78d22f172959f8c9a9037c97719e5b041dfe644ef019e7db'
